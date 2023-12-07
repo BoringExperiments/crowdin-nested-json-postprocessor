15a169c71893fce68400ee432b6ca8365dadbe79
Ushie <ushiekane@gmail.com>
2023-12-07 20:32:00 +0300

    feat: python package

---
+++ crowdin_nested_json_postprocessor/__main__.py
@@ -0,0 +1,14 @@
+import os
+
+from .postprocessor import postprocessor
+
+
+def main():
+    for file in os.listdir("input"):
+        with open(f"{os.path.join('input')}/{file}", "r", encoding="utf8") as f:
+            data = f.read()
+            remove_empty = postprocessor.remove_empty(data)
+            print(f"⚙️ {file} have been successfully formatted!")
+        with open(f"{os.path.join('output')}/{file}", "w", encoding="utf8") as f:
+            f.write(remove_empty)
+            print(f"🥞 {file} have been successfully written to output!")
--- lib/__init__.py
+++
@@ -1,2 +0,0 @@
-from .cleanup import remove_empty   # noqa: F401
-# from .formatter import format_json  # noqa: F401
